7c6dee84f311c5ec7eb797ed0cd2d5d0dbe7dd19
Ahmed-Thydjane <61056694+Ahmed-Thydjane@users.noreply.github.com>
2022-01-15 19:02:16 +0100

    Delete backend directory

--- backend/atlas_connect.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-const db_url='mongodb+srv://cheick:3AXse281qibWcFVL@cluster0.f51yr.mongodb.net/vhome?retryWrites=true&w=majority';
-mongoose.connect(db_url,{UseNewUrlParser: true, useUnifiedTopology: true});
--- backend/video_api.js
+++
@@ -1,4 +0,0 @@
-
-async function video_api (req,res){
-
-}
